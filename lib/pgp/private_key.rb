8413d9fe89623f854b6a1aa276cdd26deed19080
Scott Gonyea <me@sgonyea.com>
2012-11-16 21:03:12 -0800

    Add Ruby classes that help fetch the public / private keys from a file (or string)

--- lib/pgp/private_key.rb
+++ lib/pgp/private_key.rb
@@ -1,12 +1,33 @@
 module PGP
+  # This is more module than class. Eventually it will probably inherit from
+  #   the PGPPrivateKey class and make using it less ghoulish.
   class PrivateKey
+    include_package "org.bouncycastle.openpgp"
 
-    def self.from_file(path)
-      self.new(File.read path)
+    def self.from_string(string, key_id)
+      stream = PGP.string_to_bais(string)
+      pgp_sec = keyring_from_stream(stream)
+      sec_key = pgp_sec.get_secret_key(key_id)
+
+      sec_key.extract_private_key(nil, BC_Provider_Code) if sec_key
     end
 
-    def initialize(key)
-      # @todo ...
+    def self.from_file(filename, key_id)
+      pgp_sec = keyring_from_file(filename)
+      sec_key = pgp_sec.get_secret_key(key_id)
+
+      sec_key.extract_private_key(nil, BC_Provider_Code) if sec_key
+    end
+
+    protected
+    def self.keyring_from_file(filename)
+      file = File.open(filename)
+      keyring_from_stream(file.to_inputstream)
+    end
+
+    def self.keyring_from_stream(stream)
+      yafs = PGPUtil.get_decoder_stream(stream)
+      PGPSecretKeyRingCollection.new(yafs)
     end
 
   end
--- lib/pgp/public_key.rb
+++ lib/pgp/public_key.rb
@@ -1,12 +1,36 @@
 module PGP
+  # This is more module than class. Eventually it will probably inherit from
+  #   the PGPPublicKey class and make using it less ghoulish.
   class PublicKey
+    include_package "org.bouncycastle.openpgp"
 
-    def self.from_file(path)
-      self.new(File.read path)
+    def self.from_file(filename)
+      keys_from_file(filename).first
     end
 
-    def initialize(key)
-      # @todo ...
+    def self.keys_from_file(filename)
+      key_enumerator = keyring_from_file(filename).get_key_rings
+      encryption_keys = []
+
+      key_enumerator.each do |pk_ring|
+        pk_enumerator = pk_ring.get_public_keys
+
+        pk_enumerator.each do |key|
+          next unless key.is_encryption_key
+
+          encryption_keys << key
+        end
+      end
+
+      encryption_keys
+    end
+
+    protected
+    def self.keyring_from_file(filename)
+      file = File.open(filename)
+      yafs = PGPUtil.get_decoder_stream(file.to_inputstream)
+
+      PGPPublicKeyRingCollection.new(yafs)
     end
 
   end
