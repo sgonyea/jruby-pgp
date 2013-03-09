5db5dbf4fe52ec937a948910758a708037cc80c9
Chris Nelson <chris@gaslightsoftware.com>
2013-03-08 19:27:16 -0500

    adding a spec to prove we can do what we came here to do.

--- spec/lib/pgp/signer_spec.rb
+++ spec/lib/pgp/signer_spec.rb
@@ -4,7 +4,13 @@ describe PGP::Signer do
   let(:private_key_path)   { Fixtures_Path.join('private_key_with_passphrase.asc').to_s }
   let(:public_key_path)   { Fixtures_Path.join('public_key_with_passphrase.asc').to_s }
 
-  let(:signer) { PGP::Signer.new }
+  let(:signer) do
+    signer = PGP::Signer.new
+    signer.passphrase = "testingpgp"
+    signer.add_keys_from_file(private_key_path)
+    signer
+  end
+
   let(:unsigned_file) { Fixtures_Path.join('signed_file.txt') }
   let(:unsigned_data) { File.read(unsigned_file)}
   let(:signed_file) { Fixtures_Path.join('signed_file.txt.asc') }
@@ -15,18 +21,24 @@ describe PGP::Signer do
   end
 
   describe '#sign' do
-    context 'When the Public Key is from a file' do
 
-      before do
-        signer.passphrase = "testingpgp"
-        signer.add_keys_from_file(private_key_path)
-      end
-
-      it "signs" do
-        verifier.verify(signer.sign(unsigned_data)).should == unsigned_data
-      end
+    it "signs" do
+      verifier.verify(signer.sign(unsigned_data)).should == unsigned_data
     end
 
+  end
+
+  describe "encrypting and signing" do
+    let(:encryptor) { PGP::Encryptor.new(File.read public_key_path) }
+    let(:decryptor) do
+      decryptor = PGP::Decryptor.new
+      decryptor.passphrase = "testingpgp"
+      decryptor.add_keys_from_file(private_key_path)
+      decryptor
+    end
+    it "can decrypt and verify something that has been signed and encrypted" do
+      verifier.verify(decryptor.decrypt(encryptor.encrypt(signer.sign("something fabulous")))).should == "something fabulous"
+    end
   end
 
 end
